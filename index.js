47792497ceab68ca524090cabf830266e0cad547
juanfe1923 <felipeuribe778@gmail.com>
2026-09-08 13:04:00 -0500

    funcionamiento

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ app.use('/api/pedidos-producto', require('./routes/PedidoProducto.routes'));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
 });
